307165acd2e47af9823fa298c476b3b8a3633e73
Yobee <ksey4055@naver.com>
2024-08-13 22:37:02 +0900

    useEffect 사용법

--- src/Detail.js
+++ src/Detail.js
@@ -1,35 +1,39 @@
 /* eslint-disable */
 // 디테일페이지
 import {useParams} from "react-router-dom";
-import styled from 'styled-components';
-
-/*
-* styled 특징 해당 파일에서만 css 속성 컴포넌트를 사용할 수 있게함
-* 추가로 특정파일.module.css 파일은 특정파일 에 국한한 css 속성을 제공한다.
-* */
-
-let YelloBtn = styled.button`
-    background: ${ props => props.bg };
-    color: ${props => props.bg ==='blue' ? 'white' : 'black'};
-    padding: 10px;
-`
-let BlackBox = styled.div`
-    background: grey;
-    padding: 20px;
-`
+import {useEffect, useState} from "react";
 
 function Detail(props) {
+
+    /* 이전에는 class 만들어서, mount, update, unmount 따로 설정했음
+        이젠 useEffect 하나면 설정가능
+        1. 시간오래걸리는 작업
+        2. DB에서 데이터가져오는것
+        3. 타이머 등을 가져올 때 */
+    useEffect(() => {
+        setTimeout(()=>{
+            document.querySelector('.alert').style.display = 'none';
+        },2000);
+    });
+
+    /*const [count, setCount] = useState(0);*/
 
     let {id} = useParams();
     const product =  props.shoes.find(item => item.id === parseInt(id));
     const imgUrl = 'https://codingapple1.github.io/shop/shoes'+ (parseInt(id)+1) +'.jpg';
     return (
         <div className="container">
-            <YelloBtn bg="blue">버튼</YelloBtn>
-            <YelloBtn bg="orange">버튼</YelloBtn>
+            {/*{count}
+            <button onClick={()=>{setCount(count+1)}}>버튼</button>*/}
+
+            {/*숙제*/}
+            <div className="alert alert-warning">
+                2초 이내 구매시 할인
+            </div>
+
             <div className="row">
                 <div className="col-md-6">
-                    <img src={imgUrl} width="100%" />
+                    <img src={imgUrl} width="100%" alt="prdtImg"/>
                 </div>
                 <div className="col-md-6">
                     <h4 className="pt-5">{product.title}</h4>
