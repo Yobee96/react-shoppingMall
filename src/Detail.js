eb6ad87e53a93bfddd334ef7bf207dee358434c7
Yobee <ksey4055@naver.com>
2024-08-16 22:46:59 +0900

    props 생략 방법, 컴포넌트 활용법

--- src/Detail.js
+++ src/Detail.js
@@ -1,36 +1,30 @@
 /* eslint-disable */
-// 디테일페이지
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {Nav} from 'react-bootstrap';
 
 function Detail(props) {
 
     const [alert, setAlert] = useState(true);
     const [count, setCount] = useState(0);
     const [onlyNumber, setOnlyNumber] = useState(false);
-    
+
+    const [tab, setTab] = useState(0);
+
+
+
     useEffect(() => {
         let timer = setTimeout(()=>{
             setAlert(false);
         },2000);
 
-        // clear function : return
         return()=>{
             console.log(1);
             /*기존 타이머를 제거*/
             clearTimeout(timer);
-
-            /*기존 데이터 요청은 삭제 등등 도 가능*/
         }
     });
 
-    /* useEffect 뒤 [] 는 defendency : 선언해두면 mount 시와 해당 변수가 변경될 때만, useEffect 실행함
-    * 그리고 [] 빈값으로 두면 mount 시에만 실행하고, 변수 업데이트시에 사용안됨
-    * 
-    * return() 을 사용 가능함 return 은 useEffect가 실행되기 전에 실행됨
-    * 이전 타이머는 제거해주세요 같은 코드 실행가능
-    * 이러한 정리 함수를 Clear Function 이라함 (mount 때는 실행 안됨/unmount 때도 실행됨)
-    * */
 
     let {id} = useParams();
     const product =  props.shoes.find(item => item.id === parseInt(id));
@@ -43,25 +37,11 @@ function Detail(props) {
                 </div> :
                 null
             }
-            {count}
-            <button onClick={()=>{setCount(count+1)}}>버튼</button>
 
             <div className="row">
                 <div className="col-md-6">
                     <img src={imgUrl} width="100%" alt="prdtImg"/>
                 </div>
-                {
-                    onlyNumber ?
-                    <p style={{color : 'red'}}>숫자만 입력하세요</p>
-                    : null
-                }
-                <input className="inputOnlyNumber" onChange={(e)=>{
-                    if (isNaN(e.target.value)){
-                        setOnlyNumber(true);
-                    } else {
-                        setOnlyNumber(false);
-                    }
-                }}/>
 
                 <div className="col-md-6">
                     <h4 className="pt-5">{product.title}</h4>
@@ -70,8 +50,33 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
+
+            <Nav variant="pills" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link0" onClick={()=>{ setTab(0); }}>Option 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link1" onClick={()=>{ setTab(1); }}>Option 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link2" onClick={()=>{ setTab(2); }}>Option 3</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <TabContent tab={tab}/>
+            {/*
+            tab === 0 ? <div>내용0</div> :
+            tab === 1 ? <div>내용1</div> :
+            tab === 2 ? <div>내용2</div> : null
+            */}
+
         </div>
     )
 }
+
+function TabContent ({tab}) {
+    return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]
+}
+
 
 export default Detail;
